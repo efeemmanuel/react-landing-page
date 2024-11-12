0db83e507cf857aae59dffc920774ed90ef7d2da
efeemmanuel <83638482+efeemmanuel@users.noreply.github.com>
2024-11-12 12:16:46 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer(){
         <footer className="mt-20 border-t py-10 border-neutral-700">
             <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
 
-                <div>
+                <di>
                     <h3 className="text-md font-semibold mb-4">Resources</h3>
                     <ul className="space-y-2">
                         {resourcesLinks.map((link,index) =>(
