ea5bc5a3dfd81f379a77d29202d205cd9a9e72f4
efeemmanuel <83638482+efeemmanuel@users.noreply.github.com>
2024-11-12 12:13:57 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { resourcesLinks,platformLinks, communityLinks } from "../constants";
 
 export default function Footer(){
     return (
-        <footer className="mt-20 border-t py-10 border-neutral-70">
+        <footer className="mt-20 border-t py-10 border-neutral-700">
             <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
 
                 <div>
